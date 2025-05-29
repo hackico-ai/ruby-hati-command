64cf3bca44e979be5e57e73d2837600d052b0c39
MarieGi <giy.mariya@gmail.com>
2025-05-28 17:30:20 -0700

    upd: callee ext

--- lib/hati_command/callee.rb
+++ lib/hati_command/callee.rb
@@ -1,23 +1,31 @@
 # frozen_string_literal: true
 
 # Module for adding callable functionality
-module Callee
-  def self.included(base)
-    base.extend(ClassMethods)
-  end
+module HatiCommand
+  # Module for adding callable functionality to a class
+  module Callee
+    def self.included(base)
+      base.extend(CalleeClassMethods)
+    end
 
-  # Class methods for Callee
-  module ClassMethods
-    # Calls the instance method `call` on a new instance of the class.
-    #
-    # @param args [Array] arguments to be passed to the instance method
-    # @return [Object] the result of the instance method call
-    def call(...)
-      obj = new
+    # WIP: I know what you did last summer =)
+    def self.whoami
+      'My Name is Callee'
+    end
 
-      yield(obj) if block_given?
+    # Class methods for Callee
+    module CalleeClassMethods
+      # Calls the instance method `call` on a new instance of the class.
+      #
+      # @param args [Array] arguments to be passed to the instance method
+      # @return [Object] the result of the instance method call
+      def call(...)
+        obj = new
 
-      obj.call(...)
+        yield(obj) if block_given?
+
+        obj.call(...)
+      end
     end
   end
 end
