c6c2f28438655e77f7682f399742f2d5877f0bc8
MarieGi <giy.mariya@gmail.com>
2025-05-28 17:33:32 -0700

    add: lib aggregation

---
+++ lib/hati_command.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'hati_command/version'
+# errors
+require 'hati_command/errors/fail_fast_error'
+# result
+require 'hati_command/result'
+require 'hati_command/success'
+require 'hati_command/failure'
+# core
+require 'hati_command/callee'
+require 'hati_command/befehl'
+require 'hati_command/cmd'
